d1ed75cca5e41cfe20d2c45486dfa2912be3b09b
Orson <670407215@qq.com>
2019-10-05 17:25:52 -0500

    Initialize POJO Data Model. Closes #2.

---
+++ src/main/java/orson/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,9 @@
+package orson.springframework.sfgpetclinic.model;
+
+public class Person {
+
+	private String firstname;
+	private String lastName;
+	
+	
+}
---
+++ src/main/java/orson/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,5 @@
+package orson.springframework.sfgpetclinic.model;
+
+public class PetType {
+
+}
